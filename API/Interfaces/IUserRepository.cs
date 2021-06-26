41cca5d0bd6cb2563efef2444038c7ec89721f98
AhmedTalat <talat.salem@gmail.com>
2021-06-26 21:13:01 +0200

    Section 8

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+
+        Task<bool> SaveAllAsync();
+
+        Task<IEnumerable<AppUser>> GetAllUsersAsync();
+
+        Task<AppUser> GetUserByIdAsync(int Id);
+
+        Task<AppUser> GetUserByUsernameAsync(string Username);
+
+        Task<IEnumerable<MemberDto>> GetAllMembersAsync();
+
+        Task<MemberDto> GetMemberByUsernameAsync(string Username);
+    }
+}
