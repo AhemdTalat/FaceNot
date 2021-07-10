1b7aeb74238ad9a3f51c26edb364260ec6bbca3f
AhmedTalat <talat.salem@gmail.com>
2021-07-10 09:01:13 +0200

    Applied fix

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
+import { BusyService } from './busy.service';
 import { getPaginationHeaders, getPaginationResult } from './paginationHelper';
 
 @Injectable({
@@ -20,9 +21,10 @@ export class MessageService {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private busyService: BusyService) { }
 
   createHubConnection(user: User, otherUsername: string) {
+    this.busyService.busy();
 
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
@@ -33,7 +35,8 @@ export class MessageService {
 
     this.hubConnection
       .start()
-      .catch(error => console.log(error));
+      .catch(error => console.log(error))
+      .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', (messageThread: Message[]) => {
       this.messageThreadSource.next(messageThread);
@@ -61,6 +64,7 @@ export class MessageService {
 
   stopHubConnection() {
     if (this.hubConnection) {
+      this.messageThreadSource.next([]);
       this.hubConnection.stop().catch(error => console.log(error));
     }
   }
